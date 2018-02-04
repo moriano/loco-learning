9f5f4cc0fa8a2181b5a5a0e717c9984b902ff52f
moriano <moriano@gmail.com>
2018-02-04 17:55:35 +1100

    Completed cat vs dogs competition, final log loss is 0.221, still can be improved!

--- cats-vs-dogs/run.sh
+++ cats-vs-dogs/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-floyd run --gpu --mode jupyter --data moriano/datasets/cats-vs-dogs-augmented/1 --env tensorflow-1.2
+#floyd run --gpu --mode jupyter --data moriano/datasets/cats-vs-dogs-augmented/1 --env tensorflow-1.4
+floyd run --gpu --mode jupyter --data moriano/datasets/cats-vs-dogs-224x244x3/2 --env tensorflow-1.4
